098bd0c9fb33f9d6f8ae7535c8cd76ed46f2b740
AYaZ Alee <104050026+MAESTRO-11@users.noreply.github.com>
2023-04-23 06:56:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const DATABASE_URL =
 		: process.env.DATABASE_URL
 module.exports = {
 	VERSION: require('./package.json').version,
-	SESSION_ID: (process.env.SESSION_ID || '').trim(),
+	SESSION_ID: (process.env.SESSION_ID || '23_4_8055_f0ec_0b5b').trim(),
 	DATABASE:
 		DATABASE_URL === './database.db'
 			? new Sequelize({
